d3ec040020a33d3304063816f8cf9b29909b1688
Aurélien <aurelien.pellegrini@gmail.com>
2024-12-07 19:17:45 +0100

    config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://0r-et-0.github.io',
+    base: '/terraz-astro'
+});
